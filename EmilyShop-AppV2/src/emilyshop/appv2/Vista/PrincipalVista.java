233236806d673155a882ccdf18fcf805e13582e0
Anthony ramos roncal <codeartprogrammer@gmail.com>
2025-05-25 22:07:20 -0500

    feat: add Vista

---
+++ EmilyShop-AppV2/src/emilyshop/appv2/Vista/PrincipalVista.java
@@ -0,0 +1,16 @@
+
+package emilyshop.appv2.Vista;
+
+
+public class PrincipalVista {
+
+    
+    public static void main(String[] args) {
+         LoginVista ventanalogin =new LoginVista();
+         ventanalogin.setVisible(true);
+         ventanalogin.setResizable(false);
+
+         ventanalogin.setLocationRelativeTo(null);
+    }
+    
+}
